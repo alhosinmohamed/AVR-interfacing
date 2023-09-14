7c06f86d4ec1f0a0f4e889df1eed2b963c1f6834
Elhussein Yousef <144842500+alhosinmohamed@users.noreply.github.com>
2023-09-14 18:29:04 +0300

    Add files via upload

---
+++ drivers/KeyPad_Driver/KeyPad_Cfg.h
@@ -0,0 +1,22 @@
+/*****************************************************/
+/* Author      : alhosin                             */
+/* date        : 07/09/2023                          */
+/*****************************************************/
+#ifndef KEYPAD_CFG_H
+#define KEYPAD_CFG_H
+
+
+#define KeyPad_Port DIO_PORTA
+
+#define Row1_Pin DIO_PIN4
+#define Row2_Pin DIO_PIN5
+#define Row3_Pin DIO_PIN6
+#define Row4_Pin DIO_PIN7
+
+#define Column1_Pin DIO_PIN3
+#define Column2_Pin DIO_PIN2
+#define Column3_Pin DIO_PIN1
+#define Column4_Pin DIO_PIN0
+
+
+#endif
