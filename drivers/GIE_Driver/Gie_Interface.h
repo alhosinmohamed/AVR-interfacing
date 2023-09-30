11a4187b3ebd20c69b5bebef9800868b51feb154
Elhussein Yousef <144842500+alhosinmohamed@users.noreply.github.com>
2023-09-30 04:22:40 +0300

    Add files via upload

---
+++ drivers/GIE_Driver/Gie_Interface.h
@@ -0,0 +1,15 @@
+/*****************************************************/
+/* Author      : alhosin                             */
+/* date        : 21/09/2023                          */
+/*****************************************************/
+#ifndef GIE_INTERFACE_H_
+#define GIE_INTERFACE_H_
+
+ 
+void  GIE_voidEnable(void);
+void  GIE_voidDisable(void);
+
+
+
+
+#endif
